a81b1d1c47ed47e0d86e583c40b2e75861f67435
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-08-08 13:10:52 -0400

    Handle json.Number values.
    In order to prevent the JSON decoder from converting all values to
    `float64`, which causes a number of problems, one must set `UseNumber` on
    a JSON decoder which tells it to unmarshal as `json.Number` values.
    These can then be converted to ints, floats, or used as strings.
    
    However, although setting this value is very common, mapstructure cannot
    currently decode from a `map[string]interface{}` containing a
    `json.Number` to a struct with those values represented as int or float.
    
    This adds code and test cases to correctly decode a json.Number into an
    int or float as expected. It does not decode into uint, although this
    could be handled, since json.Number does not natively decode into an
    unsigned int.

--- mapstructure_test.go
+++ mapstructure_test.go
@@ -1,6 +1,7 @@
 package mapstructure
 
 import (
+	"encoding/json"
 	"io"
 	"reflect"
 	"sort"
@@ -9,14 +10,17 @@ import (
 )
 
 type Basic struct {
-	Vstring string
-	Vint    int
-	Vuint   uint
-	Vbool   bool
-	Vfloat  float64
-	Vextra  string
-	vsilent bool
-	Vdata   interface{}
+	Vstring     string
+	Vint        int
+	Vuint       uint
+	Vbool       bool
+	Vfloat      float64
+	Vextra      string
+	vsilent     bool
+	Vdata       interface{}
+	VjsonInt    int
+	VjsonFloat  float64
+	VjsonNumber json.Number
 }
 
 type BasicSquash struct {
@@ -109,13 +113,16 @@ func TestBasicTypes(t *testing.T) {
 	t.Parallel()
 
 	input := map[string]interface{}{
-		"vstring": "foo",
-		"vint":    42,
-		"Vuint":   42,
-		"vbool":   true,
-		"Vfloat":  42.42,
-		"vsilent": true,
-		"vdata":   42,
+		"vstring":     "foo",
+		"vint":        42,
+		"Vuint":       42,
+		"vbool":       true,
+		"Vfloat":      42.42,
+		"vsilent":     true,
+		"vdata":       42,
+		"vjsonInt":    json.Number("1234"),
+		"vjsonFloat":  json.Number("1234.5"),
+		"vjsonNumber": json.Number("1234.5"),
 	}
 
 	var result Basic
@@ -155,6 +162,18 @@ func TestBasicTypes(t *testing.T) {
 
 	if result.Vdata != 42 {
 		t.Error("vdata should be valid")
+	}
+
+	if result.VjsonInt != 1234 {
+		t.Errorf("vjsonint value should be 1234: %#v", result.VjsonInt)
+	}
+
+	if result.VjsonFloat != 1234.5 {
+		t.Errorf("vjsonfloat value should be 1234.5: %#v", result.VjsonFloat)
+	}
+
+	if !reflect.DeepEqual(result.VjsonNumber, json.Number("1234.5")) {
+		t.Errorf("vjsonnumber value should be '1234.5': %T, %#v", result.VjsonNumber, result.VjsonNumber)
 	}
 }
 
