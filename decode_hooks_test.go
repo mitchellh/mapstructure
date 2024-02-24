bec4b0532e3083001f27eda3e7e78f124822634f
tlipoca9 <tlipoca9@gmail.com>
2024-02-25 00:54:21 +0800

    feat: add StringToBasicTypesHookFunc

--- decode_hooks_test.go
+++ decode_hooks_test.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"net/netip"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
@@ -641,6 +642,557 @@ func TestStringToNetIPAddrPortHookFunc(t *testing.T) {
 			t.Fatalf("case %d: expected err %#v", i, tc.err)
 		}
 		if !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToBasicTypeHookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, strValue, "42", false},
+		{strValue, reflect.ValueOf(int8(0)), int8(42), false},
+		{strValue, reflect.ValueOf(uint8(0)), uint8(42), false},
+		{strValue, reflect.ValueOf(int16(0)), int16(42), false},
+		{strValue, reflect.ValueOf(uint16(0)), uint16(42), false},
+		{strValue, reflect.ValueOf(int32(0)), int32(42), false},
+		{strValue, reflect.ValueOf(uint32(0)), uint32(42), false},
+		{strValue, reflect.ValueOf(int64(0)), int64(42), false},
+		{strValue, reflect.ValueOf(uint64(0)), uint64(42), false},
+		{strValue, reflect.ValueOf(int(0)), int(42), false},
+		{strValue, reflect.ValueOf(uint(0)), uint(42), false},
+		{strValue, reflect.ValueOf(float32(0)), float32(42), false},
+		{strValue, reflect.ValueOf(float64(0)), float64(42), false},
+		{reflect.ValueOf("true"), reflect.ValueOf(bool(false)), true, false},
+		{strValue, reflect.ValueOf(byte(0)), byte(42), false},
+		{strValue, reflect.ValueOf(rune(0)), rune(42), false},
+		{strValue, reflect.ValueOf(complex64(0)), complex64(42), false},
+		{strValue, reflect.ValueOf(complex128(0)), complex128(42), false},
+	}
+
+	for i, tc := range cases {
+		f := StringToBasicTypeHookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToInt8HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	int8Value := reflect.ValueOf(int8(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, int8Value, int8(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), int8Value, int8(0), true},
+		{reflect.ValueOf("42.42"), int8Value, int8(0), true},
+		{reflect.ValueOf("-42"), int8Value, int8(-42), false},
+		{reflect.ValueOf("0b101010"), int8Value, int8(42), false},
+		{reflect.ValueOf("052"), int8Value, int8(42), false},
+		{reflect.ValueOf("0o52"), int8Value, int8(42), false},
+		{reflect.ValueOf("0x2a"), int8Value, int8(42), false},
+		{reflect.ValueOf("0X2A"), int8Value, int8(42), false},
+		{reflect.ValueOf("0"), int8Value, int8(0), false},
+		{reflect.ValueOf("0.0"), int8Value, int8(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToInt8HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToUint8HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	uint8Value := reflect.ValueOf(uint8(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, uint8Value, uint8(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), uint8Value, uint8(0), true},
+		{reflect.ValueOf("42.42"), uint8Value, uint8(0), true},
+		{reflect.ValueOf("-42"), uint8Value, uint8(0), true},
+		{reflect.ValueOf("0b101010"), uint8Value, uint8(42), false},
+		{reflect.ValueOf("052"), uint8Value, uint8(42), false},
+		{reflect.ValueOf("0o52"), uint8Value, uint8(42), false},
+		{reflect.ValueOf("0x2a"), uint8Value, uint8(42), false},
+		{reflect.ValueOf("0X2A"), uint8Value, uint8(42), false},
+		{reflect.ValueOf("0"), uint8Value, uint8(0), false},
+		{reflect.ValueOf("0.0"), uint8Value, uint8(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToUint8HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToInt16HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	int16Value := reflect.ValueOf(int16(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, int16Value, int16(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), int16Value, int16(0), true},
+		{reflect.ValueOf("42.42"), int16Value, int16(0), true},
+		{reflect.ValueOf("-42"), int16Value, int16(-42), false},
+		{reflect.ValueOf("0b101010"), int16Value, int16(42), false},
+		{reflect.ValueOf("052"), int16Value, int16(42), false},
+		{reflect.ValueOf("0o52"), int16Value, int16(42), false},
+		{reflect.ValueOf("0x2a"), int16Value, int16(42), false},
+		{reflect.ValueOf("0X2A"), int16Value, int16(42), false},
+		{reflect.ValueOf("0"), int16Value, int16(0), false},
+		{reflect.ValueOf("0.0"), int16Value, int16(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToInt16HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToUint16HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	uint16Value := reflect.ValueOf(uint16(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, uint16Value, uint16(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), uint16Value, uint16(0), true},
+		{reflect.ValueOf("42.42"), uint16Value, uint16(0), true},
+		{reflect.ValueOf("-42"), uint16Value, uint16(0), true},
+		{reflect.ValueOf("0b101010"), uint16Value, uint16(42), false},
+		{reflect.ValueOf("052"), uint16Value, uint16(42), false},
+		{reflect.ValueOf("0o52"), uint16Value, uint16(42), false},
+		{reflect.ValueOf("0x2a"), uint16Value, uint16(42), false},
+		{reflect.ValueOf("0X2A"), uint16Value, uint16(42), false},
+		{reflect.ValueOf("0"), uint16Value, uint16(0), false},
+		{reflect.ValueOf("0.0"), uint16Value, uint16(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToUint16HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToInt32HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	int32Value := reflect.ValueOf(int32(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, int32Value, int32(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), int32Value, int32(0), true},
+		{reflect.ValueOf("42.42"), int32Value, int32(0), true},
+		{reflect.ValueOf("-42"), int32Value, int32(-42), false},
+		{reflect.ValueOf("0b101010"), int32Value, int32(42), false},
+		{reflect.ValueOf("052"), int32Value, int32(42), false},
+		{reflect.ValueOf("0o52"), int32Value, int32(42), false},
+		{reflect.ValueOf("0x2a"), int32Value, int32(42), false},
+		{reflect.ValueOf("0X2A"), int32Value, int32(42), false},
+		{reflect.ValueOf("0"), int32Value, int32(0), false},
+		{reflect.ValueOf("0.0"), int32Value, int32(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToInt32HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToUint32HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	uint32Value := reflect.ValueOf(uint32(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, uint32Value, uint32(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), uint32Value, uint32(0), true},
+		{reflect.ValueOf("42.42"), uint32Value, uint32(0), true},
+		{reflect.ValueOf("-42"), uint32Value, uint32(0), true},
+		{reflect.ValueOf("0b101010"), uint32Value, uint32(42), false},
+		{reflect.ValueOf("052"), uint32Value, uint32(42), false},
+		{reflect.ValueOf("0o52"), uint32Value, uint32(42), false},
+		{reflect.ValueOf("0x2a"), uint32Value, uint32(42), false},
+		{reflect.ValueOf("0X2A"), uint32Value, uint32(42), false},
+		{reflect.ValueOf("0"), uint32Value, uint32(0), false},
+		{reflect.ValueOf("0.0"), uint32Value, uint32(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToUint32HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToInt64HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	int64Value := reflect.ValueOf(int64(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, int64Value, int64(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), int64Value, int64(0), true},
+		{reflect.ValueOf("42.42"), int64Value, int64(0), true},
+		{reflect.ValueOf("-42"), int64Value, int64(-42), false},
+		{reflect.ValueOf("0b101010"), int64Value, int64(42), false},
+		{reflect.ValueOf("052"), int64Value, int64(42), false},
+		{reflect.ValueOf("0o52"), int64Value, int64(42), false},
+		{reflect.ValueOf("0x2a"), int64Value, int64(42), false},
+		{reflect.ValueOf("0X2A"), int64Value, int64(42), false},
+		{reflect.ValueOf("0"), int64Value, int64(0), false},
+		{reflect.ValueOf("0.0"), int64Value, int64(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToInt64HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToUint64HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	uint64Value := reflect.ValueOf(uint64(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, uint64Value, uint64(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), uint64Value, uint64(0), true},
+		{reflect.ValueOf("42.42"), uint64Value, uint64(0), true},
+		{reflect.ValueOf("-42"), uint64Value, uint64(0), true},
+		{reflect.ValueOf("0b101010"), uint64Value, uint64(42), false},
+		{reflect.ValueOf("052"), uint64Value, uint64(42), false},
+		{reflect.ValueOf("0o52"), uint64Value, uint64(42), false},
+		{reflect.ValueOf("0x2a"), uint64Value, uint64(42), false},
+		{reflect.ValueOf("0X2A"), uint64Value, uint64(42), false},
+		{reflect.ValueOf("0"), uint64Value, uint64(0), false},
+		{reflect.ValueOf("0.0"), uint64Value, uint64(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToUint64HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToIntHookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	intValue := reflect.ValueOf(int(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, intValue, int(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), intValue, int(0), true},
+		{reflect.ValueOf("42.42"), intValue, int(0), true},
+		{reflect.ValueOf("-42"), intValue, int(-42), false},
+		{reflect.ValueOf("0b101010"), intValue, int(42), false},
+		{reflect.ValueOf("052"), intValue, int(42), false},
+		{reflect.ValueOf("0o52"), intValue, int(42), false},
+		{reflect.ValueOf("0x2a"), intValue, int(42), false},
+		{reflect.ValueOf("0X2A"), intValue, int(42), false},
+		{reflect.ValueOf("0"), intValue, int(0), false},
+		{reflect.ValueOf("0.0"), intValue, int(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToIntHookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToUintHookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42")
+	uintValue := reflect.ValueOf(uint(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, uintValue, uint(42), false},
+		{strValue, strValue, "42", false},
+		{reflect.ValueOf(strings.Repeat("42", 42)), uintValue, uint(0), true},
+		{reflect.ValueOf("42.42"), uintValue, uint(0), true},
+		{reflect.ValueOf("-42"), uintValue, uint(0), true},
+		{reflect.ValueOf("0b101010"), uintValue, uint(42), false},
+		{reflect.ValueOf("052"), uintValue, uint(42), false},
+		{reflect.ValueOf("0o52"), uintValue, uint(42), false},
+		{reflect.ValueOf("0x2a"), uintValue, uint(42), false},
+		{reflect.ValueOf("0X2A"), uintValue, uint(42), false},
+		{reflect.ValueOf("0"), uintValue, uint(0), false},
+		{reflect.ValueOf("0.0"), uintValue, uint(0), true},
+	}
+
+	for i, tc := range cases {
+		f := StringToUintHookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, tc.err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToFloat32HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42.42")
+	float32Value := reflect.ValueOf(float32(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, float32Value, float32(42.42), false},
+		{strValue, strValue, "42.42", false},
+		{reflect.ValueOf(strings.Repeat("42", 420)), float32Value, float32(0), true},
+		{reflect.ValueOf("42.42.42"), float32Value, float32(0), true},
+		{reflect.ValueOf("-42.42"), float32Value, float32(-42.42), false},
+		{reflect.ValueOf("0"), float32Value, float32(0), false},
+		{reflect.ValueOf("1e3"), float32Value, float32(1000), false},
+		{reflect.ValueOf("1e-3"), float32Value, float32(0.001), false},
+	}
+
+	for i, tc := range cases {
+		f := StringToFloat32HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToFloat64HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42.42")
+	float64Value := reflect.ValueOf(float64(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, float64Value, float64(42.42), false},
+		{strValue, strValue, "42.42", false},
+		{reflect.ValueOf(strings.Repeat("42", 420)), float64Value, float64(0), true},
+		{reflect.ValueOf("42.42.42"), float64Value, float64(0), true},
+		{reflect.ValueOf("-42.42"), float64Value, float64(-42.42), false},
+		{reflect.ValueOf("0"), float64Value, float64(0), false},
+		{reflect.ValueOf("0.0"), float64Value, float64(0), false},
+		{reflect.ValueOf("1e3"), float64Value, float64(1000), false},
+		{reflect.ValueOf("1e-3"), float64Value, float64(0.001), false},
+	}
+
+	for i, tc := range cases {
+		f := StringToFloat64HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToComplex64HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42.42+42.42i")
+	complex64Value := reflect.ValueOf(complex64(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, complex64Value, complex(float32(42.42), float32(42.42)), false},
+		{strValue, strValue, "42.42+42.42i", false},
+		{reflect.ValueOf(strings.Repeat("42", 420)), complex64Value, complex(float32(0), 0), true},
+		{reflect.ValueOf("42.42.42"), complex64Value, complex(float32(0), 0), true},
+		{reflect.ValueOf("-42.42"), complex64Value, complex(float32(-42.42), 0), false},
+		{reflect.ValueOf("0"), complex64Value, complex(float32(0), 0), false},
+		{reflect.ValueOf("0.0"), complex64Value, complex(float32(0), 0), false},
+		{reflect.ValueOf("1e3"), complex64Value, complex(float32(1000), 0), false},
+		{reflect.ValueOf("1e-3"), complex64Value, complex(float32(0.001), 0), false},
+		{reflect.ValueOf("1e3i"), complex64Value, complex(float32(0), 1000), false},
+		{reflect.ValueOf("1e-3i"), complex64Value, complex(float32(0), 0.001), false},
+	}
+
+	for i, tc := range cases {
+		f := StringToComplex64HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
+			t.Fatalf(
+				"case %d: expected %#v, got %#v",
+				i, tc.result, actual)
+		}
+	}
+}
+
+func TestStringToComplex128HookFunc(t *testing.T) {
+	strValue := reflect.ValueOf("42.42+42.42i")
+	complex128Value := reflect.ValueOf(complex128(0))
+
+	cases := []struct {
+		f, t   reflect.Value
+		result interface{}
+		err    bool
+	}{
+		{strValue, complex128Value, complex(42.42, 42.42), false},
+		{strValue, strValue, "42.42+42.42i", false},
+		{reflect.ValueOf(strings.Repeat("42", 420)), complex128Value, complex(0, 0), true},
+		{reflect.ValueOf("42.42.42"), complex128Value, complex(0, 0), true},
+		{reflect.ValueOf("-42.42"), complex128Value, complex(-42.42, 0), false},
+		{reflect.ValueOf("0"), complex128Value, complex(0, 0), false},
+		{reflect.ValueOf("0.0"), complex128Value, complex(0, 0), false},
+		{reflect.ValueOf("1e3"), complex128Value, complex(1000, 0), false},
+		{reflect.ValueOf("1e-3"), complex128Value, complex(0.001, 0), false},
+		{reflect.ValueOf("1e3i"), complex128Value, complex(0, 1000), false},
+		{reflect.ValueOf("1e-3i"), complex128Value, complex(0, 0.001), false},
+	}
+
+	for i, tc := range cases {
+		f := StringToComplex128HookFunc()
+		actual, err := DecodeHookExec(f, tc.f, tc.t)
+		if tc.err != (err != nil) {
+			t.Fatalf("case %d: expected err %#v", i, err)
+		}
+		if !tc.err && !reflect.DeepEqual(actual, tc.result) {
 			t.Fatalf(
 				"case %d: expected %#v, got %#v",
 				i, tc.result, actual)
